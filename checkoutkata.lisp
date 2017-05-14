3c58f7f28ff262cc9f835fd7614d774e3104faa7
Jacob Clark <jacob.clark@coop.co.uk>
2017-05-14 22:30:55 +0100

    Tidy up

--- checkoutkata.lisp
+++ checkoutkata.lisp
@@ -15,65 +15,76 @@
 (setf (gethash 'A *offers*) '(3 130))
 (setf (gethash 'B *offers*) '(2 45))
 
-(defun discounter(type basket threshold count)
+(defun discounted-price(type)
+  (first (last (gethash type *offers*)))
+)
+
+(defun discounted-offer(type)
+  (first (gethash type *offers*))
+)
+
+(defun discounter(type basket discount-threshold count-in-basket)
   (if 
     (and 
-      (eq 
-        (member type basket) NIL) 
-        (< count threshold))
+      (eq (member type basket) NIL) 
+      (< count-in-basket discount-threshold))
     0
     (if 
-      (eq threshold count)
-        (first (last (gethash type *offers*)))
-        (discounter 
-          type 
-          (remove type basket :count 1)
-          threshold 
-          (+ count 1)
-        )))
+      (eq 
+        discount-threshold 
+        count-in-basket)
+        (discounted-price type)
+      (discounter 
+        type 
+        (remove type basket :count 1)
+        discount-threshold 
+        (+ count-in-basket 1)
+      )))
 )
 
 (defun apply-discount(type basket)
-  (let ( (offer (gethash type *offers*)) )
-    (if (eq offer NIL)
+  (if (eq (discounted-offer type) NIL)
+    0
+    (discounter 
+      type 
+      basket 
+      (discounted-offer type)
       0
-      (discounter type basket (first offer) 0)
     )
   )
 )
 
-(defun calculate-cost(items acumulator)
-  (if (eq items NIL)
-    acumulator 
+(defun sum(items acumulator)
+  (let ((item (first items)))
+    (if (eq items NIL)
+      acumulator 
       (if 
         (eq 
-          (apply-discount 
-            (first items) 
-            items)
-          0
+          (apply-discount item items)
+          0)
+        (sum
+          (remove item items :count 1)
+          (+ (gethash item *items*) acumulator)
         )
-        (calculate-cost
-          (remove (first items) items :count 1)
-          (+ (gethash (first items) *items*) acumulator)
-        )
-        (calculate-cost
-          (remove (first items) items :count (first (gethash (first items) *offers*)))
-          (+ (apply-discount (first items) items) acumulator)
+        (sum
+          (remove item items :count (first (gethash item *offers*)))
+          (+ (apply-discount item items) acumulator)
         )
       )
     )
+  ) 
 )
 
-(defun total(items)
-  (calculate-cost items 0)
+(defun checkout(items)
+  (sum items 0)
 )
 
 (defun runtests()
-  (is (total '(A A)) 100 "total returns 100 for AA")
-  (is (total '(A A A)) 130 "total returns 130 for AAA")
-  (is (total '(A B C D)) 115 "total returns 115 for ABCD")
-  (is (total '(D A B A B A)) 190 "total returns 190 for DABABA")
-  (is (total '(A A A A A A)) 260 "total returns 260 for AAAAAA")
+  (is (checkout '(A A)) 100 "checkout returns 100 for AA")
+  (is (checkout '(A A A)) 130 "checkout returns 130 for AAA")
+  (is (checkout '(A B C D)) 115 "checkout returns 115 for ABCD")
+  (is (checkout '(D A B A B A)) 190 "checkout returns 190 for DABABA")
+  (is (checkout '(A A A A A A)) 260 "checkout returns 260 for AAAAAA")
 
   (is (apply-discount 'A '(A B A B A)) 130 "applies discount on valid basket and returns discount price")
   (is (apply-discount 'A '(A B)) 0 "does not apply discount when discount is not relevant")
