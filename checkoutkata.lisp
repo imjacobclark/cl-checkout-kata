d1825d990f590afb0e5b8fbc973dc6ac1205ccfb
Jacob Clark <jacob.clark@coop.co.uk>
2017-05-14 22:50:40 +0100

    More cleanup

--- checkoutkata.lisp
+++ checkoutkata.lisp
@@ -15,40 +15,42 @@
 (setf (gethash 'A *offers*) '(3 130))
 (setf (gethash 'B *offers*) '(2 45))
 
-(defun discounted-price(type)
-  (first (last (gethash type *offers*)))
-)
-
-(defun discounted-offer(type)
-  (first (gethash type *offers*))
-)
-
-(defun discounter(type basket discount-threshold count-in-basket)
+(defun calculate-discount(item basket discount-threshold discount-price count-in-basket)
   (if 
     (and 
-      (eq (member type basket) NIL) 
+      (eq (member item basket) NIL) 
       (< count-in-basket discount-threshold))
     0
     (if 
       (eq 
         discount-threshold 
         count-in-basket)
-        (discounted-price type)
-      (discounter 
-        type 
-        (remove type basket :count 1)
-        discount-threshold 
+        discount-price
+      (calculate-discount 
+        item 
+        (remove item basket :count 1)
+        discount-threshold
+        discount-price 
         (+ count-in-basket 1)
       )))
 )
 
-(defun apply-discount(type basket)
-  (if (eq (discounted-offer type) NIL)
+(defun discounted-price(item)
+  (first (last (gethash item *offers*)))
+)
+
+(defun discounted-offer(item)
+  (first (gethash item *offers*))
+)
+
+(defun apply-discount(item basket)
+  (if (eq (discounted-offer item) NIL)
     0
-    (discounter 
-      type 
+    (calculate-discount 
+      item 
       basket 
-      (discounted-offer type)
+      (discounted-offer item)
+      (discounted-price item)
       0
     )
   )
@@ -89,6 +91,9 @@
   (is (apply-discount 'A '(A B A B A)) 130 "applies discount on valid basket and returns discount price")
   (is (apply-discount 'A '(A B)) 0 "does not apply discount when discount is not relevant")
   (is (apply-discount 'D '(D)) 0 "does not apply discount when discount is not relevant")
+
+  (is (calculate-discount 'A '(A A) 2 100 0) 100 "Calculate discount calculates some discount for valid basket")
+  (is (calculate-discount 'A '(A) 2 100 0) 0 "Calculate discount calculates no discount for invalid basket")
 )
 
 (runtests)
